c202adc545af19cabda91180fc7fec9c88d34a92
slokamtech123 <slokamtechprivatelimited@gmail.com>
2022-07-13 18:03:37 +0530

    Added: Creating a Table

--- src/com/niha/MySqlTest.java
+++ src/com/niha/MySqlTest.java
@@ -4,4 +4,10 @@ public class MySqlTest {
     create database employee_payroll_service;
     show databases;
     use employee_payroll_service;
+
+    create table employee_payroll(
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            name varchar(50),
+    salary INT,
+    start_date DATE NOT NULL);
 }
